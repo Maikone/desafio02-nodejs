75811e791a74971d8b623cb1a64cf8d7515e63cb
maiKone <michaelmrmj@gmail.com>
2023-07-23 02:44:24 -0300

    .

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,5 @@
+import { response } from "express";
+
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -7,10 +9,21 @@ interface IRequest {
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  // eslint-disable-next-line prettier/prettier
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ email, name }: IRequest): User {
     // Complete aqui
+
+    const userAlreadyExists = this.usersRepository.findByEmail(email);
+    if (userAlreadyExists) {
+      throw response.status(400).json({ error: " Eroo" });
+    }
+    const user = this.usersRepository.create({
+      name,
+      email,
+    });
+    return user;
   }
 }
 
